a48400c0399087cd045b3553eed31a048c9f9c7b
Rajat chauhan <rajatchauhan9690@gmail.com>
2026-01-21 20:54:14 +0530

    add backend deploy url

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,8 +1,7 @@
 export const USER_API_END_POINT =
-  "https://jobportal-100.onrender.com/api/v1/user";
-export const JOB_API_END_POINT =
-  "https://jobportal-100.onrender.com/api/v1/job";
+  "https://naukriportal.onrender.com/api/v1/user";
+export const JOB_API_END_POINT = "https://naukriportal.onrender.com/api/v1/job";
 export const APPLICATION_API_END_POINT =
-  "https://jobportal-100.onrender.com/api/v1/application";
+  "https://naukriportal.onrender.com/api/v1/application";
 export const COMPANY_API_END_POINT =
-  "https://jobportal-100.onrender.com/api/v1/company";
+  "https://naukriportal.onrender.com/api/v1/company";
